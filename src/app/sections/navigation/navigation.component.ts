95bc8d2b45718c6bcf462637890eb891cd2ac5ee
Amruth Pillai <im.amruth@gmail.com>
2019-01-31 15:16:48 +0530

    add animation using anime.js

--- src/app/sections/navigation/navigation.component.ts
+++ src/app/sections/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import anime from 'node_modules/animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-navigation',
@@ -10,6 +11,11 @@ export class NavigationComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    anime({
+      targets: 'nav',
+      translateY: '100',
+      easing: 'easeInOutQuad'
+    });
   }
 
 }
