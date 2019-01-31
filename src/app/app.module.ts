e1ca46b98061f727ba3d6b97808c1d6fa6b1a971
Amruth Pillai <im.amruth@gmail.com>
2019-01-31 12:03:26 +0530

    almost completed landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { IconsModule } from './modules/icons.module';
+
 import { LandingComponent } from './sections/landing/landing.component';
 import { HexagonComponent } from './sections/landing/hexagon/hexagon.component';
 import { NavigationComponent } from './sections/navigation/navigation.component';
@@ -17,9 +19,13 @@ import { NavigationComponent } from './sections/navigation/navigation.component'
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    IconsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/modules/icons.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faFacebookF,
+  faInstagram,
+  faTwitter,
+  faGithubAlt,
+  faGooglePlusG,
+  faDribbble,
+  faLinkedinIn
+} from '@fortawesome/free-brands-svg-icons';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [FontAwesomeModule]
+})
+export class IconsModule {
+  constructor() {
+    library.add(
+      faFacebookF,
+      faInstagram,
+      faTwitter,
+      faGithubAlt,
+      faGooglePlusG,
+      faDribbble,
+      faLinkedinIn
+    );
+  }
+}
